6eb14933172466f67c89c87c9ecee0d98f8135c9
Bill Desmarais <bill@witsaid.com>
2012-01-21 11:32:32 -0500

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BoardgameEventer::Application.config.secret_token = 'bd3609ffb794c0fce3978256846b9cd357432415f7ae19e3efcfddbd116ce4cfddf5dd49c624e10c6a63e7ed9f8b81d846f71c644ddf970c6a926f5dea5a8745'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+BoardgameEventer::Application.config.session_store :cookie_store, key: '_boardgame_eventer_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# BoardgameEventer::Application.config.session_store :active_record_store
