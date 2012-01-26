75ed08d042459fcf90ab0dbb3d2416ab75e2ee3b
Bill Desmarais <bill@witsaid.com>
2012-01-25 19:06:28 -0500

    Mark's TODO: make these pass with his xml magicks

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -1,5 +1,34 @@
 require 'spec_helper'
 
 describe Game do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "the class should have a class method to create itself from xml" do
+    subject { Game }
+
+    specify { should respond_to(:create_from_bgg_xml) }
+  end
+  
+  describe "the class should be creatable from valid xml" do
+    #caution - all examples are horrible. Figuring out bgg xml is SEP, sez me.
+    let(:xml_str) do
+      <<-XML
+        <game>
+          <id>
+            2
+          </id>
+          <name>
+            Settlers of Japan
+          </name>
+        </game>
+      XML
+    end
+    let(:xml_tree) { SomethingXMLISH.new(xml_str) }
+
+    subject { Game.create_from_bgg_xml(xml_tree) }
+    
+    it { should be }   #should exist, akwardly worded
+    it { should be_valid }
+  end
+
+
 end
